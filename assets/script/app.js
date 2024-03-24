06dd77cff12195f849c60ef6df163e1a5a8586d6
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-24 16:48:13 -0500

    implement count decrement

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,17 +6,22 @@ import * as utils from './utils.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
+/*  Selectors                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 const hintGenerator = utils.select('.hint-generator');
 const guessInput = utils.select('.guess-input');
 const guessesRemaining = utils.select('.guesses-remaining');
+const restartButton = utils.select('.restart-button');
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Play Game                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 function generateRandomNumber() {
   return utils.randomNumber(1, 50);
 }
 
 let randomNumber = generateRandomNumber();
+let guesses = 5;
 
 function handleGuess() {
   let inputValue = parseFloat(guessInput.value.trim());
@@ -25,14 +30,29 @@ function handleGuess() {
       hintGenerator.innerHTML = 'Well done! You guessed the right number!';
     } else if (inputValue > randomNumber) {
       hintGenerator.innerHTML = 'My number is lower.';
+      guesses--;
+      guessesRemaining.innerHTML = guesses;
     } else {
-      hintGenerator.innerHTML = 'My number is higher.'
+      hintGenerator.innerHTML = 'My number is higher.';
+      guesses--;
+      guessesRemaining.innerHTML = guesses;
     }
   }
   else {
     hintGenerator.innerHTML = 'Please enter a valid number between 1 and 50';
   }
 }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Restart Game                                         */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* function restartGame() {
+
+} */
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Event Listeners                                       */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 utils.listen('keydown', guessInput, event => {
   if (event.key === 'Enter') {
